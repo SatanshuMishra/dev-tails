52a493d8293d9d9f88031929b43068ed5a6f8388
Satanshu Mishra <satanshumishra@outlook.com>
2024-03-01 21:14:00 -0800

    Astro.Config, TableOfContents, BlogLayout, & More
    - Update Astro.Config to use dependencies important for auto-generating TableOfContents.
    - Moved dedundant frontmatter code to BlogLayout from [slug].astro.
    - Implemented TableOfContent and TableOfContentHeader components to be used to generate and diaplay the TableOfContents.
    - Implemented frontmatter and heading usage on BlogLayout.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,42 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import remarkToc from "remark-toc";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import { fromHtmlIsomorphic } from "hast-util-from-html-isomorphic";
+// import expressiveCode from "astro-expressive-code";
+// import metaTags from "astro-meta-tags";
+import vercel from "@astrojs/vercel/static";
 
-import vercel from "@astrojs/vercel/serverless";
-import vercelStatic from '@astrojs/vercel/static';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
-  output: "static",
-  adapter: vercelStatic()
+	output: "static",
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
+	markdown: {
+		remarkPlugins: [
+			remarkToc,
+		],
+		rehypePlugins: [
+			rehypeHeadingIds,
+			[
+				rehypeAutolinkHeadings,
+				{
+					behavior: "append",
+					content: fromHtmlIsomorphic(
+						'<span class="ri-link-m ri-sm anchor-link" style="margin-left: 0.4rem;"></span>'
+					),
+				},
+			],
+		],
+	},
+	integrations: [
+		tailwind(),
+ 		// expressiveCode(astroExpressiveCodeOptions),
+		mdx(),
+		// metaTags(),
+	],
 });
